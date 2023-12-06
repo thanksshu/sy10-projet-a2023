b783cd656a41e557883410e19df2068ac8992a14
cherrpey <cherrpey@SMESTACAO01G.ad.utc>
2023-12-06 15:58:25 +0100

    deuxieme ajout des classe pour les systèmes floues

---
+++ fis/vars/evenement_prevu.m
@@ -0,0 +1,5 @@
+var_evenement_prevu = fisvar([0 30], "Name", "evenement_prevu");
+var_evenement_prevu = addMF(var_evenement_prevu, "trapmf", [0 0 6 10], "Name", "peu");
+var_evenement_prevu = addMF(var_evenement_prevu, "trapmf", [8 10 14 16], "Name", "moyen");
+var_evenement_prevu = addMF(var_evenement_prevu, "trapmf", [12 16 100 100], "Name", "beaucoup");
+
