b36afb41815375627cb62b8554528a809a89e179
cherrpey <cherrpey@SMESTACAO01G.ad.utc>
2023-12-06 16:23:38 +0100

    ajout sf03

---
+++ fis/sf03.m
@@ -0,0 +1,15 @@
+run("vars/nb_arret.m");
+run("vars/personnes_autres_lignes.m");
+
+
+run("vars/nb_p_changer_ligne.m");
+
+fis_sf03 = mamfis("Name", "sf03");
+fis_sf03.inputs(1) = var_nb_arret;
+fis_sf03.inputs(2) = var_personnes_autres_lignes;
+
+fis_sf03.outputs(1) = var_nb_p_changer_ligne;
+
+table = [1 1 2 ; 1 2 3 ; 2 3 3]
+
+fis_sf03 = addRule(fis_sf03, gen_rules_from_table(table));
