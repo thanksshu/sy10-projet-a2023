7a92ccb30154df5711e53becf09674e1b0459011
hanshang <shangyuan.han@etu.utc.fr>
2023-12-07 10:28:58 +0100

    Add SF04

--- fis/sf04.m
+++ fis/sf04.m
@@ -1,14 +1,14 @@
-run("vars/ressentie_meteo.m");
-run("vars/envie_de_sortir.m");
+run("vars/pollution.m");
+run("vars/prix_essence.m");
 
-run("vars/choix_prendre_le_bus.m");
+run("vars/coeff_econo_ecolo.m");
 
-fis_sf08 = mamfis("Name", "sf07");
-fis_sf08.inputs(1) = var_ressentie_meteo;
-fis_sf08.inputs(2) = var_envie_de_sortir;
+fis_sf04 = mamfis("Name", "sf04");
+fis_sf04.inputs(1) = var_pollution;
+fis_sf04.inputs(2) = var_prix_essence;
 
-fis_sf08.outputs(1) = var_choix_prendre_le_bus;
+fis_sf04.outputs(1) = var_coeff_econo_ecolo;
 
-table = [1 1 2 1; 1 3 3 2; 2 4 4 2; 2 3 3 2; 1 2 2 1];
+table = [1 2 2; 1 2 3; 2 2 3];
 
-fis_sf08 = addRule(fis_sf08, gen_rules_from_table(table));
+fis_sf04 = addRule(fis_sf04, gen_rules_from_table(table));
