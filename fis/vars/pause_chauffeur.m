72ed8035b2e7dc0401326482f0754c1f0e2cbed0
cherrpey <cherrpey@SMESTAENS12M.ad.utc>
2023-12-13 15:38:23 +0100

    changement classe pause chauffeur

--- fis/vars/pause_chauffeur.m
+++ fis/vars/pause_chauffeur.m
@@ -1,4 +1,4 @@
 var_pause_chauffeur = fisvar([0 20], "Name", "pause_chauffeur");
-var_pause_chauffeur = addMF(var_pause_chauffeur, "trapmf", [0 0 8 10], "Name", "courte");
-var_pause_chauffeur = addMF(var_pause_chauffeur, "trapmf", [8 12 15 16], "Name", "moyenne");
-var_pause_chauffeur = addMF(var_pause_chauffeur, "trapmf", [15 16 18 18], "Name", "longue");
+var_pause_chauffeur = addMF(var_pause_chauffeur, "trapmf", [0 0 20 23], "Name", "courte");
+var_pause_chauffeur = addMF(var_pause_chauffeur, "trapmf", [21 23 25 28], "Name", "moyenne");
+var_pause_chauffeur = addMF(var_pause_chauffeur, "trapmf", [25 28 30 45], "Name", "longue");
