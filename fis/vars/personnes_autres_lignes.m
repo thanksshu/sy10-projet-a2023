c12df16e505db120cea9c4fa01d7b22da8ecc357
cherrpey <cherrpey@SMESTAENS09E.ad.utc>
2023-12-19 20:16:55 +0100

    changement classe personne changement de ligne

--- fis/vars/personnes_autres_lignes.m
+++ fis/vars/personnes_autres_lignes.m
@@ -1,4 +1,4 @@
-var_personnes_autres_lignes = fisvar([0 1000], "Name", "personnes_autres_lignes");
-var_personnes_autres_lignes = addMF(var_personnes_autres_lignes, "trapmf", [0 0 120 200], "Name", "peu");
-var_personnes_autres_lignes = addMF(var_personnes_autres_lignes, "trapmf", [140 200 360 440], "Name", "moyen");
-var_personnes_autres_lignes = addMF(var_personnes_autres_lignes, "trapmf", [400 500 1000 1000], "Name", "beaucoup");
+var_personnes_autres_lignes = fisvar([0 2000], "Name", "personnes_autres_lignes");
+var_personnes_autres_lignes = addMF(var_personnes_autres_lignes, "trapmf", [0 0 400 600], "Name", "peu");
+var_personnes_autres_lignes = addMF(var_personnes_autres_lignes, "trapmf", [500 700 800 1000], "Name", "moyen");
+var_personnes_autres_lignes = addMF(var_personnes_autres_lignes, "trapmf", [800 1000 2000 2000], "Name", "beaucoup");
