a2291a2be0666f04d0f7c02d2a6a1241fa7ef250
cherrpey <cherrpey@SMESTAENS09C.ad.utc>
2023-12-19 15:59:17 +0100

    mise à jour des tests

--- fis/vars/pollution.m
+++ fis/vars/pollution.m
@@ -1,4 +1,4 @@
-var_pollution = fisvar([0 300], "Name", "pollution");
+var_pollution = fisvar([0 500], "Name", "pollution");
 var_pollution = addMF(var_pollution, "trapmf", [0 0 30 60], "Name", "faible");
 var_pollution = addMF(var_pollution, "trapmf", [50 70 90 110], "Name", "moyenne");
-var_pollution = addMF(var_pollution, "trapmf", [90 140 300 300], "Name", "forte");
+var_pollution = addMF(var_pollution, "trapmf", [90 140 500 500], "Name", "forte");
