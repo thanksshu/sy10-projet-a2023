91fbefdd46275112ce9bbc75fc373c8be043cf52
Shangyuan Han <shangyuan.han@etu.utc.fr>
2023-12-20 16:29:53 +0100

    Fix round strategy

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ close all;
 
 %% 
 
-entree = jsondecode(fileread("tests/test_1.json"));
+entree = jsondecode(fileread("tests/test_5.json"));
 
 addpath("fis");
 addpath("fis/vars");
@@ -144,16 +144,21 @@ nb_bus_2h = round(defuzz(range, discrete_nb_bus_2h, "centroid"));
 
 disp("nb_bus_2h = " + nb_bus_2h);
 disp("nb_chauffeur_2h = " + nb_chauffeur_2h);
+disp(nb_bus_envoye_pour_chaque_ligne);
 
 if min(nb_bus_2h, nb_chauffeur_2h) < 6
     disp("pas assez de bus ou de chauffeur pour pouvoir assurer le service");
 else
-    disp(nb_bus_envoye_pour_chaque_ligne);
+    i = 1;
     while (sum(round(nb_bus_envoye_pour_chaque_ligne)) <= min(nb_bus_2h, nb_chauffeur_2h)) == 0
-       nb_bus_envoye_pour_chaque_ligne = ...
-           nb_bus_envoye_pour_chaque_ligne(nb_bus_envoye_pour_chaque_ligne > 0.51) - resolution;
+        if i > nb_ligne
+            i = 1;
+        end
+        if nb_bus_envoye_pour_chaque_ligne(i) > 0.51
+            nb_bus_envoye_pour_chaque_ligne(i) = nb_bus_envoye_pour_chaque_ligne(i) - resolution;
+        end
+        i = i + 1;
     end
-    
-    disp(round(nb_bus_envoye_pour_chaque_ligne));
 end
 
+disp(round(nb_bus_envoye_pour_chaque_ligne));
